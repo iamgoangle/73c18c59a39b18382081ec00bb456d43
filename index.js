5f5cee447087150f6338232bf56681b3a8cf4c6a
teerapong.s <teerapong.s@linecorp.com>
2022-02-01 13:48:33 +0700

    feat(handler): add handler logic to transform json to structure

--- index.js
+++ index.js
@@ -56,6 +56,71 @@ async function transformJsonHandler(request, h) {
   try {
     request.logger.info("payload %s", JSON.stringify(httpReqPayload));
 
+    let response = [];
+    let normalizeToArrObjectsModel = [];
+
+    // normalize the data model to array of object
+    // remove the hash key
+    normalizeToArrObjectsModel = (() => {
+      let normalizeData = [];
+      for (const key in httpReqPayload) {
+        for (let i = 0; i < httpReqPayload[key].length; i++) {
+          let nodeItem = httpReqPayload[key][i];
+
+          normalizeData.push({
+            ...nodeItem,
+          });
+        }
+      }
+
+      return normalizeData;
+    })();
+
+    let dataSetIsInvalid = (() => {
+      let findDataSetOverMaxRule = normalizeToArrObjectsModel.filter(
+        (item) => item.level > maxDepthLevel
+      );
+
+      return findDataSetOverMaxRule.length > 0;
+    })();
+
+    if (dataSetIsInvalid) {
+      console.error(
+        `The dataset max depth level is invalid. We are limit ${maxDepthLevel} for depth level`
+      );
+      return;
+    }
+
+    // finding the <root> nodes
+    // filter the node items that no pointer to parent_id
+    response = normalizeToArrObjectsModel.filter(
+      (item) => item.parent_id === null
+    );
+
+    // associate sub-child item with <root> node item
+    let subChildResponse = [];
+    for (const rootNode of response) {
+      findSubChildItemsByParentId = normalizeToArrObjectsModel
+        .filter((item) => item.level === 1)
+        .filter((item) => item.parent_id === rootNode.id);
+
+      let leafItems = [];
+      for (const subChildItem of findSubChildItemsByParentId) {
+        leafItems = normalizeToArrObjectsModel
+          .filter((item) => subChildItem.id === item.parent_id)
+          .filter((item) => item.level == 2);
+
+        subChildResponse.push({
+          id: subChildItem.id,
+          title: subChildItem.title,
+          level: subChildItem.level,
+          children: leafItems,
+          parent_id: subChildItem.parent_id,
+        });
+      }
+
+      rootNode.children = subChildResponse;
+    }
 
     return h.response(response).code(200);
   } catch (e) {

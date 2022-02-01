7192e1e23bc34388bfbad2fa001a15ceeed20eb9
teerapong.s <teerapong.s@linecorp.com>
2022-02-01 16:21:13 +0700

    test(success): add success case

--- server.test.js
+++ server.test.js
@@ -108,4 +108,130 @@ describe("JSON Transform Handler", () => {
 
     expect(data.statusCode).toBe(400);
   });
+
+  it("should get json organized structure sucessfully", async () => {
+    const payload = {
+      0: [
+        {
+          id: 10,
+          title: "House",
+          level: 0,
+          children: [],
+          parent_id: null,
+        },
+      ],
+      1: [
+        {
+          id: 12,
+          title: "Red Roof",
+          level: 1,
+          children: [],
+          parent_id: 10,
+        },
+        {
+          id: 18,
+          title: "Blue Roof",
+          level: 1,
+          children: [],
+          parent_id: 10,
+        },
+        {
+          id: 13,
+          title: "Wall",
+          level: 1,
+          children: [],
+          parent_id: 10,
+        },
+      ],
+      2: [
+        {
+          id: 17,
+          title: "Blue Window",
+          level: 2,
+          children: [],
+          parent_id: 12,
+        },
+        {
+          id: 16,
+          title: "Door",
+          level: 2,
+          children: [],
+          parent_id: 13,
+        },
+        {
+          id: 15,
+          title: "Red Window",
+          level: 2,
+          children: [],
+          parent_id: 12,
+        },
+      ],
+    };
+
+    const expectedResult = [
+      {
+        id: 10,
+        title: "House",
+        level: 0,
+        children: [
+          {
+            id: 12,
+            title: "Red Roof",
+            level: 1,
+            children: [
+              {
+                id: 17,
+                title: "Blue Window",
+                level: 2,
+                children: [],
+                parent_id: 12,
+              },
+              {
+                id: 15,
+                title: "Red Window",
+                level: 2,
+                children: [],
+                parent_id: 12,
+              },
+            ],
+            parent_id: 10,
+          },
+          {
+            id: 18,
+            title: "Blue Roof",
+            level: 1,
+            children: [],
+            parent_id: 10,
+          },
+          {
+            id: 13,
+            title: "Wall",
+            level: 1,
+            children: [
+              {
+                id: 16,
+                title: "Door",
+                level: 2,
+                children: [],
+                parent_id: 13,
+              },
+            ],
+            parent_id: 10,
+          },
+        ],
+        parent_id: null,
+      },
+    ];
+
+    const options = {
+      method: "POST",
+      url: "/v1/transform/json",
+      payload: JSON.stringify(payload),
+    };
+
+    const data = await server.inject(options);
+
+    expect(data.statusCode).toBe(200);
+    expect(data.result).toStrictEqual(expectedResult);
+  });
 });
